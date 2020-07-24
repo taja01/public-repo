947ef06ebf41f1d3b151b5c73b93bd693876c327
taja01 <tamas.jarvas@betssongroup.com>
2020-07-24 15:30:32 +0200

    add kata project

---
+++ Benchmarks/src/KataBenchmarks/Program.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace KataBenchmarks
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            BenchmarkRunner.Run<Bench>();
+        }
+    }
+
+    internal class TestData
+    {
+        public const string TestText = "This website is for losers LOL!";
+    }
+
+    internal static class TestMethods
+    {
+        public static string DisemvowelRegex1(string str)
+        {
+            return Regex.Replace(str, "[aeiou]", "", RegexOptions.IgnoreCase);
+        }
+
+        public static string DisemvowelRegex2(string str)
+        {
+            return Regex.Replace(str, "[euioa]", "", RegexOptions.IgnoreCase); ;
+        }
+        public static string DisemvowelLinQ1(string str)
+        {
+            return string.Concat(str.Where(ch => !"aeiouAEIOU".Contains(ch)));
+        }
+        public static string DisemvowelLinQ2(string str)
+        {
+            return new string(str.Where(c => !"aeiuoAEIOU".Contains(c)).ToArray());
+        }
+    }
+
+    public class Bench
+    {
+        [Benchmark]
+        public bool TestDisemvowelRegex1() => "Ths wbst s fr lsrs LL!".Equals(TestMethods.DisemvowelRegex1(TestData.TestText));
+
+        [Benchmark]
+        public bool TestDisemvowelRegex2() => "Ths wbst s fr lsrs LL!".Equals(TestMethods.DisemvowelRegex2(TestData.TestText));
+
+        [Benchmark]
+        public bool TestDisemvowelLinQ1() => "Ths wbst s fr lsrs LL!".Equals(TestMethods.DisemvowelLinQ1(TestData.TestText));
+
+        [Benchmark]
+        public bool TestDisemvowelLinQ2() => "Ths wbst s fr lsrs LL!".Equals(TestMethods.DisemvowelLinQ2(TestData.TestText));
+    }
+}
---
+++ SolutionForFun/test/CodeWarsXUnitTest/Tests/DisemvowelTests.cs
@@ -0,0 +1,14 @@
+﻿using CodeWarsXUnitTest.Tasks;
+using Xunit;
+
+namespace CodeWarsXUnitTest.Tests
+{
+    public class DisemvowelTests
+    {
+        [Fact]
+        public void ShouldRemoveAllVowels()
+        {
+            Assert.Equal("Ths wbst s fr lsrs LL!", Kata.Disemvowel("This website is for losers LOL!"));
+        }
+    }
+}
