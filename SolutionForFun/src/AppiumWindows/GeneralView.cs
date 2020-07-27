3eb32527bc8e9f6b7fcc9b4bf9e96c275a074672
taja01 <tamas.jarvas@betssongroup.com>
2020-07-27 12:50:01 +0200

    appium windows short demo

---
+++ SolutionForFun/src/AppiumWindows/GeneralView.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium.Appium.Windows;
+
+namespace AppiumWindows
+{
+    public class GeneralView
+    {
+        private readonly WindowsDriver<WindowsElement> driver;
+
+        public GeneralView(WindowsDriver<WindowsElement> driver)
+        {
+            this.driver = driver;
+        }
+
+        public WindowsElement Number_0 => this.driver.FindElementByAccessibilityId("num0Button");
+        public WindowsElement Number_1 => this.driver.FindElementByAccessibilityId("num1Button");
+        public WindowsElement Number_2 => this.driver.FindElementByAccessibilityId("num2Button");
+        public WindowsElement Number_3 => this.driver.FindElementByAccessibilityId("num3Button");
+        public WindowsElement Number_4 => this.driver.FindElementByAccessibilityId("num4Button");
+        public WindowsElement Number_5 => this.driver.FindElementByAccessibilityId("num5Button");
+        public WindowsElement Number_6 => this.driver.FindElementByAccessibilityId("num6Button");
+        public WindowsElement Number_7 => this.driver.FindElementByAccessibilityId("num7Button");
+        public WindowsElement Number_8 => this.driver.FindElementByAccessibilityId("num8Button");
+        public WindowsElement Number_9 => this.driver.FindElementByAccessibilityId("num9Button");
+        public WindowsElement PlusButton => this.driver.FindElementByAccessibilityId("plusButton");
+        public WindowsElement EqualButton => this.driver.FindElementByAccessibilityId("equalButton");
+        public WindowsElement ResultTextLabel => this.driver.FindElementByAccessibilityId("CalculatorResults");
+
+
+    }
+}
---
+++ SolutionForFun/src/AppiumWindows/Program.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Linq;
+
+namespace AppiumWindows
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Before run: C:\Program Files (x86)\Windows Application Driver run WinAppDriver.exe
+            WindowsDriver<WindowsElement> driver;
+
+            AppiumOptions appOptions = new AppiumOptions();
+
+            appOptions.AddAdditionalCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+            appOptions.AddAdditionalCapability("deviceName", "WindowsPC");
+            driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appOptions);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            var genView = new GeneralView(driver);
+
+            genView.Number_1.Click();
+            genView.PlusButton.Click();
+            genView.Number_2.Click();
+            genView.PlusButton.Click();
+            genView.Number_3.Click();
+            genView.EqualButton.Click();
+
+            Assert.AreEqual("6", genView.ResultTextLabel.Text.Split(':').Last().Trim());
+
+            driver.Close();
+        }
+    }
+}
