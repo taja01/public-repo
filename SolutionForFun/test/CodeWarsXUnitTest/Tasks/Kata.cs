458a9b74200fd4b30c0fe012aaf16940dd4a1f38
taja01 <tamas.jarvas@betssongroup.com>
2020-07-27 07:15:14 +0200

    monday kata #1

--- SolutionForFun/test/CodeWarsXUnitTest/Tasks/Kata.cs
+++ SolutionForFun/test/CodeWarsXUnitTest/Tasks/Kata.cs
@@ -13,8 +13,21 @@ namespace CodeWarsXUnitTest.Tasks
          */
         public static string Disemvowel(string str)
         {
-            var s = str.Where(c => !"aeiuoAEIOU".Contains(c)).ToArray();
-            return new string(s);
+            return new string(str.Where(c => !"aeiuoAEIOU".Contains(c)).ToArray());
+        }
+
+        /*
+         * Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers differs from the others.
+         * Bob observed that one number usually differs from the others in evenness.
+         * Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
+         * ! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
+         */
+        public static bool XO(string input)
+        {
+            var xCount = input.ToLower().Count(c => c == 'x');
+            var oCount = input.ToLower().Count(c => c == 'o');
+
+            return xCount == oCount;
         }
     }
 }
---
+++ SolutionForFun/test/CodeWarsXUnitTest/Tests/XoTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWarsXUnitTest.Tasks;
+using Xunit;
+
+namespace CodeWarsXUnitTest.Tests
+{
+    public class XoTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(Kata.XO("xo"));
+            Assert.True(Kata.XO("xxOo"));
+            Assert.False(Kata.XO("xxxm"));
+            Assert.False(Kata.XO("Oo"));
+            Assert.False(Kata.XO("ooom"));
+        }
+
+    }
+}
