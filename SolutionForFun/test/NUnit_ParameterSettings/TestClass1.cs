be4348ea0362088096f04aef03eb299c100493b3
taja01 <tamas.jarvas@betssongroup.com>
2020-08-31 15:17:20 +0200

    add  runsettings example

---
+++ SolutionForFun/test/NUnit_ParameterSettings/TestClass1.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace NUnit_ParameterSettings
+{
+    [TestFixture]
+    public class TestClass1
+    {
+        [Category("default")]
+        [Test]
+        public void TestMethod()
+        {
+            //RUN: dotnet test NUnit_ParameterSettings.dll --filter TestCategory=default -s local.runsettings
+            var device = TestContext.Parameters["Device"];
+            var env = TestContext.Parameters["Environment"];
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual("Test", env);
+                Assert.AreEqual("Desktop", device);
+            });
+
+        }
+
+        [Category("prod")]
+        [Test]
+        public void TestMethod2()
+        {
+            //RUN: dotnet test NUnit_ParameterSettings.dll --filter TestCategory=prod -s prod.runsettings
+            var device = TestContext.Parameters["Device"];
+            var env = TestContext.Parameters["Environment"];
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual("Prod", env);
+                Assert.AreEqual("Mobile", device);
+            });
+        }
+
+    }
+}
