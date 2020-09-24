466d9e51f6f6717dfec0361704bfa93d9f250db9
taja01 <tamas.jarvas@betssongroup.com>
2020-09-24 17:02:54 +0200

    compare string -per  marketcode

---
+++ SolutionForFun/test/CommonTest/Helper.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace CommonTest
+{
+    public static class Helper
+    {
+        //https://docs.microsoft.com/en-us/globalization/locale/sorting-and-string-comparison
+        public static bool IsInAlphabeticalOrder(IList<string> list, string cultureInfo, out string message)
+        {
+            var compareInfo = CultureInfo.GetCultureInfo(cultureInfo).CompareInfo;
+            message = null;
+            var current = list.First();
+            foreach (var item in list.Skip(1))
+            {
+                var next = item;
+                var sc1 = compareInfo.GetSortKey(current);
+                var sc2 = compareInfo.GetSortKey(next);
+                var result = SortKey.Compare(sc1, sc2);
+
+                if (result > 0)
+                {
+                    message = $"Alphabetical order problem: {next} is before the {current}";
+                    return false;
+                }
+
+                current = next;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ SolutionForFun/test/CommonTest/String/CollectionTest.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CommonTest.String
+{
+    [TestFixture]
+    public class CollectionTest
+    {
+        [Test]
+        public void Test0()
+        {
+            var collection = new List<string> { "a", "b", "c" };
+            Assert.IsTrue(Helper.IsInAlphabeticalOrder(collection, "en-GB", out var message));
+            Assert.IsNull(message);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var collection = new List<string> { "Brazil", "Chile", "Egypt", "Italy", "Russia", "UEFA" };
+            Assert.IsTrue(Helper.IsInAlphabeticalOrder(collection, "en-GB", out var message));
+            Assert.IsNull(message);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var collection = new List<string> { "Brazil U20", "Chile Primera Division", "Egypt Premier League", "Italy Coppa Primavera", "Russia Regional Leagues", "UEFA Europa League" };
+            Assert.IsTrue(Helper.IsInAlphabeticalOrder(collection, "en-GB", out var message));
+            Assert.IsNull(message);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var collection = new List<string> { "Brazil U20", "Chile Primera Division", "UEFA Europa League", "Egypt Premier League", "Italy Coppa Primavera", "Russia Regional Leagues" };
+            Assert.IsFalse(Helper.IsInAlphabeticalOrder(collection, "en-GB", out var message));
+            Assert.IsNotEmpty(message);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var collection = new List<string> { "Κρίκετ", "Μπάσκετ", "Πινγκ-Πονγκ", "Τένις", "Χόκεϊ επί πάγου", "Esports" };
+            Assert.IsFalse(Helper.IsInAlphabeticalOrder(collection, "el-CY", out var message));
+            Assert.IsNotEmpty(message);
+        }
+
+        [Test]
+        public void Test5()
+        {
+            var collection = new List<string> { "Esports", "Κρίκετ", "Μπάσκετ", "Πινγκ-Πονγκ", "Τένις", "Χόκεϊ επί πάγου" };
+            Assert.IsTrue(Helper.IsInAlphabeticalOrder(collection, "el-CY", out var message));
+            Assert.IsNull(message);
+        }
+    }
+}
