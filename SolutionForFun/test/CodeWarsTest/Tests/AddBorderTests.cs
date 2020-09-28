2b54634d9da8742ba59731b2e978d024f83c1c09
taja01 <tamas.jarvas@betssongroup.com>
2020-09-28 08:49:29 +0200

    Monday RAW split kata.. add AddBoarder method and tests

---
+++ SolutionForFun/test/CodeWarsTest/Tests/AddBorderTests.cs
@@ -0,0 +1,47 @@
+﻿using CodeWarsTests.Tasks;
+using NUnit.Framework;
+
+namespace CodeWarsTests.Tests
+{
+    [TestFixture]
+    public class AddBorderTests
+    {
+        [Test]
+        public void Test1()
+        {
+            var picture = new[]
+            {
+                "a",
+            };
+
+            var expected_array = new[]
+            {
+                "***",
+                "*a*",
+                "***"
+            };
+
+            CollectionAssert.AreEqual(expected_array, Kata.AddBorder(picture));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var picture = new[]
+            {
+                "abc",
+                "ded"
+            };
+
+            var expected_array = new[]
+            {
+                "*****",
+                "*abc*",
+                "*ded*",
+                "*****"
+            };
+
+            CollectionAssert.AreEqual(expected_array, Kata.AddBorder(picture));
+        }
+    }
+}
