0e3793b34ff138ea78950a43a087fff665b0fcb1
taja01 <x-tamas.jarvas@wizzair.com>
2021-01-05 10:03:16 +0100

    Tuesday #2

---
+++ SolutionForFun/test/CodeWarsTest/Tasks/Kata5.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeWarsTests.Tasks
+{
+    public partial class Kata
+    {
+        public static int ExtraNumber(int a, int b, int c)
+        {
+            return a ^ b ^ c;
+
+            //return a == b
+            //    ? c
+            //    : a == c ? b : a;
+        }
+    }
+}
---
+++ SolutionForFun/test/CodeWarsTest/Tests/ExtraNumberTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWarsTests.Tasks;
+using NUnit.Framework;
+
+namespace CodeWarsTests.Tests
+{
+    [TestFixture]
+    public class ExtraNumberTests
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Assert.AreEqual(7, Kata.ExtraNumber(2, 7, 2));
+            Assert.AreEqual(3, Kata.ExtraNumber(3, 2, 2));
+            Assert.AreEqual(1, Kata.ExtraNumber(5, 5, 1));
+            Assert.AreEqual(3, Kata.ExtraNumber(5001, 3, 5001));
+        }
+    }
+}
