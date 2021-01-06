0aeac00c43eda03ffd131575e56d2d0f47831954
taja01 <x-tamas.jarvas@wizzair.com>
2021-01-06 09:58:10 +0100

    Wednesday

--- SolutionForFun/test/CodeWarsTest/Tasks/Kata5.cs
+++ SolutionForFun/test/CodeWarsTest/Tasks/Kata5.cs
@@ -2,6 +2,12 @@
 {
     public partial class Kata
     {
+
+        /*
+         * You're given three integers, a, b and c.
+         * It is guaranteed that two of these integers are equal to each other.
+         * What is the value of the third integer?
+         */
         public static int ExtraNumber(int a, int b, int c)
         {
             return a ^ b ^ c;
@@ -10,5 +16,18 @@
             //    ? c
             //    : a == c ? b : a;
         }
+
+        /*
+         * Given integers a and b, determine whether the following pseudocode results in an infinite loop
+         * while a is not equal to b do
+         * increase a by 1
+         *  decrease b by 1
+         * Assume that the program is executed on a virtual machine which can store arbitrary long numbers and execute forever.
+         */
+        public static bool IsInfiniteProcess(int a, int b)
+        {
+            return (a > b || (a + b) % 2 != 0);
+        }
+
     }
 }
