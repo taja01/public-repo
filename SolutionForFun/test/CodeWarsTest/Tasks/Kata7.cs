8626b28689159d644abd0473bbcc78ae3d564d23
taja01 <x-tamas.jarvas@wizzair.com>
2021-09-07 10:24:51 +0200

    simple kata task

---
+++ SolutionForFun/test/CodeWarsTest/Tasks/Kata7.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWarsTests.Tasks
+{
+    public partial class Kata
+    {
+        //Create a function called shortcut to remove all the lowercase vowels in a given string.
+        public static string Shortcut(string input)
+        {
+            //var vowels = "aeiou";
+            //return new string(input.Where(c => !vowels.Contains(c)).ToArray());
+
+            return Regex.Replace(input, "[aiuoe]", "");
+        }
+    }
+}
