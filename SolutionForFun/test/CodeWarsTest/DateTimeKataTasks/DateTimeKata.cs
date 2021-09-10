e3ac59d05688fc31cdb0c689d7ab89ed4f048d66
taja01 <x-tamas.jarvas@wizzair.com>
2021-09-10 08:48:11 +0200

    working days

---
+++ SolutionForFun/test/CodeWarsTest/DateTimeKataTasks/DateTimeKata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CodeWarsTests.DateTimeKataTasks
+{
+    public class DateTimeKata
+    {
+        /*
+         * As a good developer you have to practice a lot. One way to achieve this is to finish one kata every day after work.
+         * But of course you won't have time for that every day. 
+         * So maybe you skip one week and think you can do two katas every day in the following week. 
+         * But of course you lose track of how many katas you should have finished until today.
+         * To control yourself, write a function which gets the start date of your codewars registration and returns how many katas you should have finished until today. We will ignore public holidays.
+         */
+        public static int SolvedKatasSince(DateTime registrationDate, DateTime currentDate)
+        {
+            var counter = 0;
+            for (var d = registrationDate; d < currentDate; d = d.AddDays(1))
+            {
+                var b = d.DayOfWeek;
+                if (b != DayOfWeek.Saturday && b != DayOfWeek.Sunday)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
---
+++ SolutionForFun/test/CodeWarsTest/DateTimeKataTests/SolvedKatasSinceTests.cs
@@ -0,0 +1,40 @@
+﻿using CodeWarsTests.DateTimeKataTasks;
+using NUnit.Framework;
+using System;
+
+namespace CodeWarsTests.DateTimeKataTests
+{
+    [TestFixture]
+    public class SolvedKatasSinceTests
+    {
+        [Test]
+        public void One_Day_Should_Return_1_Finished_Kata()
+        {
+            Assert.AreEqual(1, DateTimeKata.SolvedKatasSince(new DateTime(2018, 01, 01), new DateTime(2018, 01, 2)));
+        }
+
+        [Test]
+        public void One_Week_Should_Return_5_Finished_Katas()
+        {
+            Assert.AreEqual(5, DateTimeKata.SolvedKatasSince(new DateTime(2018, 01, 01), new DateTime(2018, 01, 08)));
+        }
+
+        [Test]
+        public void Four_Weeks_Should_Return_20_Finished_Katas()
+        {
+            Assert.AreEqual(20, DateTimeKata.SolvedKatasSince(new DateTime(2018, 01, 01), new DateTime(2018, 01, 29)));
+        }
+
+        [Test]
+        public void April_2018_Should_Return_21_Finished_Katas()
+        {
+            Assert.AreEqual(21, DateTimeKata.SolvedKatasSince(new DateTime(2018, 04, 01), new DateTime(2018, 05, 01)));
+        }
+
+        [Test]
+        public void Year_2017_Should_Return_260_Finished_Katas()
+        {
+            Assert.AreEqual(260, DateTimeKata.SolvedKatasSince(new DateTime(2017, 01, 01), new DateTime(2017, 12, 31)));
+        }
+    }
+}
