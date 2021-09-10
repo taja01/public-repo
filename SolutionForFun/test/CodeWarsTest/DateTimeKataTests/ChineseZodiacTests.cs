014417077fb44045d0d56d44c4167df4b82cc357
taja01 <x-tamas.jarvas@wizzair.com>
2021-09-10 10:49:08 +0200

    friday

---
+++ SolutionForFun/test/CodeWarsTest/DateTimeKataTests/ChineseZodiacTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWarsTests.DateTimeKataTasks;
+using NUnit.Framework;
+
+namespace CodeWarsTests.DateTimeKataTests
+{
+    [TestFixture]
+    public class ChineseZodiacTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("Wood Snake", DateTimeKata.ChineseZodiac(1965));
+            Assert.AreEqual("Earth Tiger", DateTimeKata.ChineseZodiac(1938));
+            Assert.AreEqual("Earth Tiger", DateTimeKata.ChineseZodiac(1998));
+            Assert.AreEqual("Fire Monkey", DateTimeKata.ChineseZodiac(2016));
+            Assert.AreEqual("Wood Rat", DateTimeKata.ChineseZodiac(1924));
+            Assert.AreEqual("Earth Monkey", DateTimeKata.ChineseZodiac(1968));
+            Assert.AreEqual("Water Dog", DateTimeKata.ChineseZodiac(2162));
+        }
+    }
+}
