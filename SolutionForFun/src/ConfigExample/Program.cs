835a4d32f65718fe132e8c8eb34c295d7ab26088
taja01 <x-tamas.jarvas@wizzair.com>
2021-09-29 16:38:36 +0200

    config example #1

---
+++ SolutionForFun/src/ConfigExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace ConfigExample
+{
+    class Program
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+        static void Main(string[] args)
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile($"appsettings.{env}.json", optional: true);
+
+            Configuration = builder.Build();
+
+
+            Console.WriteLine(Configuration["WhereIam"]);
+        }
+    }
+}
