4af13fc4a66900dcaf70b5a6422d4d62a5a476aa
taja01 <x-tamas.jarvas@wizzair.com>
2021-09-29 15:09:24 +0200

    big boom

---
+++ SolutionForFun/test/CodeWarsTest/ListsKata/Kata.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace CodeWarsTests.ListsKata
+{
+    class Kata
+    {
+        //Find the mean (average) of a list of numbers in an array.
+        public static int FindAverage(int[] nums)
+        {
+            return (int)nums.Average();
+        }
+    }
+}
---
+++ SolutionForFun/test/CodeWarsTest/Tests/ListKataTests/GrassHopperTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWarsTests.ListsKata;
+using NUnit.Framework;
+
+namespace CodeWarsTests.Tests.ListKataTests
+{
+    [TestFixture]
+    public class GrassHopperTests
+    {
+        [Test]
+        public void FirstTest()
+        {
+            Assert.AreEqual(1, Kata.FindAverage(new[] { 1 }));
+        }
+
+        [Test]
+        public void SecondTest()
+        {
+            Assert.AreEqual(4, Kata.FindAverage(new[] { 1, 3, 5, 7 }));
+        }
+
+        [Test]
+        public void ThirdTest()
+        {
+            Assert.AreEqual(-5, Kata.FindAverage(new[] { -10, -5, -5, 0 }));
+        }
+
+        [Test]
+        public void FourthTest()
+        {
+            Assert.AreEqual(0, Kata.FindAverage(new[] { 0 }));
+        }
+    }
+}
