1af50c2d8ab35b7072230483957fcd5727e929c2
taja01 <x-tamas.jarvas@wizzair.com>
2021-09-30 15:18:18 +0200

    add example

---
+++ SolutionForFun/test/TestProjectWithConfig/MyTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace TestProjectWithConfig
+{
+    [TestFixture]
+    public class MyTest : TestSetupBase
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Assert.NotNull(Configuration);
+            Assert.AreNotEqual("none", Configuration["WhereIam"]);
+            Assert.AreEqual(EnvironmentName, Configuration["WhereIam"]);
+        }
+    }
+}
---
+++ SolutionForFun/test/TestProjectWithConfig/TestSetupBase.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using System.IO;
+
+namespace TestProjectWithConfig
+{
+    public abstract class TestSetupBase
+    {
+        protected static string EnvironmentName { get; private set; }
+        protected static IConfigurationRoot Configuration { get; private set; }
+
+        [OneTimeSetUp]
+        public void SetupTestRun()
+        {
+            // COMMAND LINE USAGE: dotnet test -v normal -- TestRunParameters.Parameter(name=\\\"environment\\\",value=\\\"DEV\\\")
+            EnvironmentName = TestContext.Parameters["environment"];
+
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .Build();
+        }
+    }
+
+    //[TestFixture]
+    //public class TestServiceSetupBase
+    //{
+    //    public static string EnvironmentName { get; private set; }
+    //    public static IHostBuilder HostBuilder { get; private set; }
+    //    public static IConfigurationRoot Configuration { get; private set; }
+    //    public static IHost Host { get; private set; }
+
+    //    [OneTimeSetUp]
+    //    public void SetupTest()
+    //    {
+    //        EnvironmentName = TestContext.Parameters["environment"];
+
+    //        HostBuilder = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder();
+
+    //        HostBuilder.UseEnvironment(EnvironmentName);
+
+    //        HostBuilderContext hostBuilderContext;
+
+    //        HostBuilder.ConfigureServices((context) =>
+    //        {
+    //            hostBuilderContext = context;
+
+    //            Configuration = new ConfigurationBuilder()
+    //            .SetBasePath(Directory.GetCurrentDirectory())
+    //            .AddJsonFile("appsettings.json")
+    //            .AddJsonFile($"appsettings.{hostBuilderContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    //            .Build();
+
+    //        });
+
+    //        Host = HostBuilder.Build();
+    //    }
+    //}
+}
