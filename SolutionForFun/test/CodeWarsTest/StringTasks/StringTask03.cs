9abbb2ddefaa78ca3dd06cc9c4d60c42a641252d
taja01 <jarvas.tamas@gmail.com>
2022-01-11 22:28:43 +0100

    tuesday #3

--- SolutionForFun/test/CodeWarsTest/StringTasks/StringTask03.cs
+++ SolutionForFun/test/CodeWarsTest/StringTasks/StringTask03.cs
@@ -99,5 +99,18 @@ namespace CodeWarsTests.StringTasks
             //    }).ToDictionary(x => x.Key, x => x.Count);
             return message.GroupBy(c => c).ToDictionary(x => x.Key, x => x.Count());
         }
+
+        /*
+         * https://www.codewars.com/kata/557af9418895e44de7000053
+         * Create a function that takes a string and an integer (n).
+         * The function should return a string that repeats the input string n number of times.
+         * If anything other than a string is passed in you should return "Not a string"
+         * "Hi", 2 --> "HiHi"
+         * 1234, 5 --> "Not a string"
+         */
+        public static string RepeatString(object toRepeat, int n)
+        {
+            return toRepeat is string ? string.Concat(Enumerable.Repeat(toRepeat, n)) : "Not a string";
+        }
     }
 }
---
+++ SolutionForFun/test/CodeWarsTest/StringTests/RepeatStringTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWarsTests.StringTasks;
+using NUnit.Framework;
+
+namespace CodeWarsTests.StringTests
+{
+    [TestFixture]
+    public class RepeatStringTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(Kata.RepeatString("*", 3), Is.EqualTo("***"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.That(Kata.RepeatString("Hi", 2), Is.EqualTo("HiHi"));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.That(Kata.RepeatString(1234, 3), Is.EqualTo("Not a string"));
+        }
+    }
+}
