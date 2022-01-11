a27e75ba1367f197c222c53d200bdb2783bc1d54
taja01 <jarvas.tamas@gmail.com>
2022-01-11 21:48:32 +0100

    tuesday #1

--- SolutionForFun/test/CodeWarsTest/StringTasks/StringTask03.cs
+++ SolutionForFun/test/CodeWarsTest/StringTasks/StringTask03.cs
@@ -68,5 +68,18 @@ namespace CodeWarsTests.StringTasks
 
             return a.Select(x => vowels.Contains(x) ? char.ConvertFromUtf32((int)x) : x).ToArray();
         }
+
+        /*
+         * https://www.codewars.com/kata/5302d846be2a9189af0001e4
+         * Create a method sayHello/say_hello/SayHello that takes as input a name, city, and state to welcome a person. 
+         * Note that name will be an array consisting of one or more values that should be joined together with one space between each, 
+         * and the length of the name array in test cases will vary.
+         * Kata.SayHello(new String[]{"John", "Smith"}, "Phoenix", "Arizona")
+         * This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+         */
+        public static string SayHello(string[] name, string city, string state)
+        {
+            return $"Hello, {string.Join(" ", name)}! Welcome to {city}, {state}!";
+        }
     }
 }
---
+++ SolutionForFun/test/CodeWarsTest/StringTests/SayHelloTests.cs
@@ -0,0 +1,15 @@
+﻿using CodeWarsTests.StringTasks;
+using NUnit.Framework;
+
+namespace CodeWarsTests.StringTests
+{
+    [TestFixture]
+    public class SayHelloTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("Hello, John Smith! Welcome to Phoenix, Arizona!", Kata.SayHello(new string[] { "John", "Smith" }, "Phoenix", "Arizona"));
+        }
+    }
+}
