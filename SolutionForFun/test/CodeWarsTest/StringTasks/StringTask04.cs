79b94a9168575e02b0d64c3ff4df8b61e301f241
taja01 <jarvas.tamas@gmail.com>
2022-01-12 13:44:41 +0100

    Wednesday #10

--- SolutionForFun/test/CodeWarsTest/StringTasks/StringTask04.cs
+++ SolutionForFun/test/CodeWarsTest/StringTasks/StringTask04.cs
@@ -154,5 +154,21 @@ namespace CodeWarsTests.StringTasks
 
             return $"{a.Max()} {a.Min()}";
         }
+
+        /*
+         * https://www.codewars.com/kata/56747fd5cb988479af000028
+         * You are going to be given a word. Your job is to return the middle character of the word.
+         * If the word's length is odd, return the middle character. 
+         * If the word's length is even, return the middle 2 characters.
+         * #Examples:
+         * Kata.getMiddle("test") should return "es"
+         * Kata.getMiddle("testing") should return "t"
+         * Kata.getMiddle("middle") should return "dd"
+         * Kata.getMiddle("A") should return "A"
+         */
+        public static string GetMiddle(string s)
+        {
+            return s.Length % 2 == 0 ? s.Substring(s.Length / 2 - 1, 2) : s.Substring(s.Length / 2, 1);
+        }
     }
 }
---
+++ SolutionForFun/test/CodeWarsTest/StringTests/GetMiddleTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWarsTests.StringTasks;
+using NUnit.Framework;
+
+namespace CodeWarsTests.StringTests
+{
+    [TestFixture]
+    public class GetMiddleTests
+    {
+        [TestCase("es", "test")]
+        [TestCase("t", "testing")]
+        [TestCase("dd", "middle")]
+        [TestCase("A", "A")]
+        public void GenericTests(string expected, string text)
+        {
+            Assert.AreEqual(expected, Kata.GetMiddle(text));
+        }
+    }
+}
