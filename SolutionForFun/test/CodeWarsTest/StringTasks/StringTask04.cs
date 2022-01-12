2fb22eb2ca19c74513ed79b1b6b9201b04a706ad
taja01 <jarvas.tamas@gmail.com>
2022-01-12 13:30:50 +0100

    Wednesday #8

--- SolutionForFun/test/CodeWarsTest/StringTasks/StringTask04.cs
+++ SolutionForFun/test/CodeWarsTest/StringTasks/StringTask04.cs
@@ -125,5 +125,23 @@ namespace CodeWarsTests.StringTasks
                 ? 0
                 : 100 * (double)cases / s.Count(char.IsNumber);
         }
+
+        /*
+         * https://www.codewars.com/kata/5a19701d80171fd71d000029
+         * This cipher involves taking each character of a string and multiplying their char codes by 6.
+         * For example, `Hello World!` would become `ưɞʈʈʚÀȊʚʬʈɘÆ`.
+         * You must write two functions:
+         * `encode` to encode a given string,
+         * `decode` to decode a given string.
+         */
+        public static string Encode(string str)
+        {
+            return string.Concat(str.Select(x => char.ConvertFromUtf32(x * 6)));
+        }
+
+        public static string Decode(string str)
+        {
+            return string.Concat(str.Select(x => char.ConvertFromUtf32(x / 6)));
+        }
     }
 }
---
+++ SolutionForFun/test/CodeWarsTest/StringTests/EncodeDecodeTests.cs
@@ -0,0 +1,16 @@
+﻿using CodeWarsTests.StringTasks;
+using NUnit.Framework;
+
+namespace CodeWarsTests.StringTests
+{
+    [TestFixture]
+    public class EncodeDecodeTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.That(Kata.Encode("Hello World!"), Is.EqualTo("ưɞʈʈʚÀȊʚʬʈɘÆ"));
+            Assert.That(Kata.Decode("ưɞʈʈʚÀȊʚʬʈɘÆ"), Is.EqualTo("Hello World!"));
+        }
+    }
+}
