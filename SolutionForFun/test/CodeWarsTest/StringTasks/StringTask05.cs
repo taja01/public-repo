d3ec430fde111d5749a5be6ca0d51d17a65e880d
taja01 <jarvas.tamas@gmail.com>
2022-01-13 21:33:41 +0100

    Thursday #2

---
+++ SolutionForFun/test/CodeWarsTest/StringTasks/StringTask05.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+
+namespace CodeWarsTests.StringTasks
+{
+    public partial class Kata
+    {
+        /*
+        * https://www.codewars.com/kata/5b5e0c0d83d64866bc00001d
+        * You have a string of numbers; starting with the third number each number is the result of an operation performed using the previous two numbers.
+        * Complete the function which returns a string of the operations in order and separated by a comma and a space, e.g. "subtraction, subtraction, addition"
+        */
+        public static string SayMeOperations(string stringNumbers)
+        {
+            var arr = stringNumbers.Split(' ').Select(x => int.Parse(x)).ToList();
+            //var operation = new List<string>();
+
+            //for (int i = 1; i < arr.Count() - 1; i++)
+            //{
+            //    if (arr[i - 1] + arr[i] == arr[i + 1])
+            //    {
+            //        operation.Add("addition");
+            //    }
+            //    else if (arr[i - 1] * arr[i] == arr[i + 1])
+            //    {
+            //        operation.Add("multiplication");
+            //    }
+            //    else if (arr[i - 1] - arr[i] == arr[i + 1])
+            //    {
+            //        operation.Add("subtraction");
+            //    }
+            //    else
+            //    {
+            //        operation.Add("division");
+            //    }
+            //}
+
+            //return string.Join(", ", operation);
+
+            return string.Join(", ", Enumerable.Range(0, arr.Count() - 2).Select(i =>
+                        arr[i] + arr[i + 1] == arr[i + 2] ? "addition" :
+                        arr[i] - arr[i + 1] == arr[i + 2] ? "subtraction" :
+                        arr[i] * arr[i + 1] == arr[i + 2] ? "multiplication" : "division"));
+        }
+
+    }
+}
---
+++ SolutionForFun/test/CodeWarsTest/StringTests/SayMeOperationsTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWarsTests.StringTasks;
+using NUnit.Framework;
+
+namespace CodeWarsTests.StringTests
+{
+    [TestFixture]
+    public class SayMeOperationsTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("addition", Kata.SayMeOperations("2 2 4"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual("division, subtraction, multiplication, subtraction", Kata.SayMeOperations("10 2 5 -3 -15 12"));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual("subtraction, multiplication, addition", Kata.SayMeOperations("9 4 5 20 25"));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual("addition, addition, addition", Kata.SayMeOperations("1 2 3 5 8"));
+        }
+    }
+}
