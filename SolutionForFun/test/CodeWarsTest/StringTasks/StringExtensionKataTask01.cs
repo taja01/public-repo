db58bfeb4b5b43c8801c3a5948c838ac9e292ef6
taja01 <jarvas.tamas@gmail.com>
2022-01-13 21:11:18 +0100

    Thursday #1

---
+++ SolutionForFun/test/CodeWarsTest/StringTasks/StringExtensionKataTask01.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+namespace CodeWarsTests.StringTasks
+{
+    public static class StringExtensionKataTask01
+    {
+        /*
+         * * https://www.codewars.com/kata/5c81aaf99043851c65ce7f5b
+        * Your task is to write an extension method for the String class called Score(), that returns an integer score for any given string.
+        * The score is calculated thus:
+        * Each letter is given a numeric value, where A = 1, B = 2, up through Z = 26 (non-case-sensitive). 
+        * The score is the sum of all values.
+        * Ignore any non-alpha characters in the string. 
+        * If there are no alpha characters in the string, the score is 0.
+        */
+        public static int Score(this string s)
+        {
+            return s.ToLower()
+                    .Where(c => char.IsLetter(c))
+                    .Sum(x => x - 96);
+        }
+
+    }
+}
---
+++ SolutionForFun/test/CodeWarsTest/StringTests/ScoreExtensionTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWarsTests.StringTasks;
+using NUnit.Framework;
+
+namespace CodeWarsTests.StringTests
+{
+    [TestFixture]
+    public class ScoreExtensionTests
+    {
+        [TestCase(6, "ABC")]
+        [TestCase(96, "Knowledge")]
+        [TestCase(98, "Hard Work")]
+        [TestCase(0, ".,<>?")]
+        [TestCase(0, "")]
+        public void TestMethod(int score, string text)
+        {
+            Assert.AreEqual(score, text.Score());
+        }
+    }
+}
