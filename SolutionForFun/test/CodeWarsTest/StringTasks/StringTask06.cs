0b49ecbf3f9ea526ad66420ec3b6e6385d36e8e1
taja01 <jarvas.tamas@gmail.com>
2022-01-21 22:07:16 +0100

    friday!

---
+++ SolutionForFun/test/CodeWarsTest/StringTasks/StringTask06.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+namespace CodeWarsTests.StringTasks
+{
+    public partial class Kata
+    {
+        /*
+         * https://www.codewars.com/kata/5a262cfb8f27f217f700000b
+         * In this Kata, you will be given two strings a and b and your task will be to return the characters that are not common in the two strings.
+         */
+        public static string Solve(string a, string b)
+        {
+            return string.Concat(string.Concat(a.Where(x => !b.Contains(x)))) + string.Concat(string.Concat(b.Where(x => !a.Contains(x))));
+        }
+    }
+}
---
+++ SolutionForFun/test/CodeWarsTest/StringTests/SolveTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWarsTests.StringTasks;
+using NUnit.Framework;
+
+namespace CodeWarsTests.StringTests
+{
+    [TestFixture]
+    public class SolveTests
+    {
+        [TestCase("xyab", "xzca", "ybzc")]
+        [TestCase("xyabb", "xzca", "ybbzc")]
+        [TestCase("abcd", "xyz", "abcdxyz")]
+        [TestCase("xxx", "xzca", "zca")]
+        public void TestMethod(string a, string b, string result)
+        {
+            Assert.That(Kata.Solve(a, b), Is.EqualTo(result));
+        }
+    }
+}
