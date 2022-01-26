c4150556f9c36ecc357d235415090f03b661f9b8
taja01 <jarvas.tamas@gmail.com>
2022-01-26 23:56:23 +0100

    w #last

--- SolutionForFun/test/CodeWarsTest/Tasks/Kata7.cs
+++ SolutionForFun/test/CodeWarsTest/Tasks/Kata7.cs
@@ -92,5 +92,15 @@ namespace CodeWarsTests.Tasks
         {
             return Math.Abs(dadYears - sonYears * 2);
         }
+
+        /*
+         * https://www.codewars.com/kata/5b077ebdaf15be5c7f000077
+         * Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...".
+         * Input will always be valid, i.e. no negative integers.
+         */
+        public static string CountSheep(int n)
+        {
+            return string.Concat(Enumerable.Range(1, n).Select(i => $"{i} sheep..."));
+        }
     }
 }
