0e03dbef27a8e23eb64b74ede4fd6e8522b8a1aa
taja01 <jarvas.tamas@gmail.com>
2022-01-26 23:44:31 +0100

    w #2

--- SolutionForFun/test/CodeWarsTest/Tasks/Kata7.cs
+++ SolutionForFun/test/CodeWarsTest/Tasks/Kata7.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace CodeWarsTests.Tasks
@@ -81,6 +82,15 @@ namespace CodeWarsTests.Tasks
             //return sum;
 
             return games.Sum(x => x[0] == x[2] ? 1 : x[0] > x[2] ? 3 : 0);
+        }
+
+        /*
+         * https://www.codewars.com/kata/5b853229cfde412a470000d0
+         * Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+         */
+        public static int TwiceAsOld(int dadYears, int sonYears)
+        {
+            return Math.Abs(dadYears - sonYears * 2);
         }
     }
 }
---
+++ SolutionForFun/test/CodeWarsTest/Tests/TwiceAsOldTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWarsTests.Tasks;
+using NUnit.Framework;
+
+namespace CodeWarsTests.Tests
+{
+    [TestFixture]
+    public class TwiceAsOldTests
+    {
+        [TestCase(30, 30, 0)]
+        [TestCase(16, 30, 7)]
+        [TestCase(15, 45, 30)]
+        public void SampleTests(int expected, int dadYears, int sonYears)
+        {
+            Assert.AreEqual(expected, Kata.TwiceAsOld(dadYears, sonYears));
+
+        }
+    }
+}
