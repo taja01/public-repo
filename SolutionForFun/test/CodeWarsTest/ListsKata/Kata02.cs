9bce3153d35a20cdd12024047fba63871d8c0741
taja01 <jarvas.tamas@gmail.com>
2022-01-28 22:57:33 +0100

    list #5

--- SolutionForFun/test/CodeWarsTest/ListsKata/Kata02.cs
+++ SolutionForFun/test/CodeWarsTest/ListsKata/Kata02.cs
@@ -76,5 +76,15 @@ namespace CodeWarsTests.ListsKata
                 ? lst.Max() - lst.Min()
                 : 0;
         }
+
+        /*
+         * https://www.codewars.com/kata/56b1eb19247c01493a000065
+         * Given a list of integers values, your job is to return the sum of the values; however,
+         * if the same integer value appears multiple times in the list, you can only count it once in your sum.
+         */
+        public static int? UniqueSum(List<int> lst)
+        {
+            return lst.Any() ? lst.Distinct().Sum() : (int?)null;
+        }
     }
 }
---
+++ SolutionForFun/test/CodeWarsTest/Tests/ListKataTests/UniqueSumTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsTests.ListsKata;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CodeWarsTests.Tests.ListKataTests
+{
+    [TestFixture]
+    public class UniqueSumTests
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Assert.AreEqual(6, Kata.UniqueSum(new List<int>() { 1, 2, 3 }));
+            Assert.AreEqual(12, Kata.UniqueSum(new List<int>() { 1, 3, 8, 1, 8 }));
+            Assert.AreEqual(-1, Kata.UniqueSum(new List<int>() { -1, -1, 5, 2, -7 }));
+        }
+
+        [Test]
+        public void EmptyTest()
+        {
+            Assert.Null(Kata.UniqueSum(new List<int>() { }));
+        }
+    }
+}
