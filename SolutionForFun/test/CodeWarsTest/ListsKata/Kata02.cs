2dfbdbfce08e2e468d68a73ade93b94151e97c4e
taja01 <jarvas.tamas@gmail.com>
2022-01-28 22:26:57 +0100

    omg

--- SolutionForFun/test/CodeWarsTest/ListsKata/Kata02.cs
+++ SolutionForFun/test/CodeWarsTest/ListsKata/Kata02.cs
@@ -16,5 +16,20 @@ namespace CodeWarsTests.ListsKata
 
             return listOfItems.OfType<int>();
         }
+
+        /*
+         * https://www.codewars.com/kata/55fd2d567d94ac3bc9000064
+         * Given the triangle of consecutive odd numbers:
+         *            1
+         *        3     5
+         *      7     9    11
+         *      Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+         *      1 -->  1
+         *      2 --> 3 + 5 = 8
+        */
+        public static long RowSumOddNumbers(long n)
+        {
+            return n * n * n;
+        }
     }
 }
---
+++ SolutionForFun/test/CodeWarsTest/Tests/ListKataTests/RowSumOddNumbersTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeWarsTests.ListsKata;
+using NUnit.Framework;
+
+namespace CodeWarsTests.Tests.ListKataTests
+{
+    [TestFixture]
+    public class RowSumOddNumbersTests
+    {
+        [TestCase(1, 1)]
+        [TestCase(8, 2)]
+        [TestCase(27, 3)]
+        [TestCase(74088, 42)]
+        public void TestMethod(long expected, long row)
+        {
+            Assert.AreEqual(expected, Kata.RowSumOddNumbers(row));
+        }
+    }
+}
