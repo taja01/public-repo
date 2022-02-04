8b3aff4382da59892fcae0b4c28bbbff3252d5dd
taja01 <jarvas.tamas@gmail.com>
2022-02-04 21:39:00 +0100

    long week, sorry

---
+++ SolutionForFun/test/CodeWarsTest/Tasks/Kata8.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace CodeWarsTests.Tasks
+{
+    public partial class Kata
+    {
+        //https://www.codewars.com/kata/57a2013acf1fa5bfc4000921
+        //Write a function which calculates the average of the numbers in a given list.
+        // Should return 0 on empty array
+        public static double FindAverage(double[] array)
+        {
+            return array.Length > 0
+                ? array.Average()
+                : 0;
+        }
+    }
+}
---
+++ SolutionForFun/test/CodeWarsTest/Tests/GeneralKataTests/FindAverageTests.cs
@@ -0,0 +1,22 @@
+﻿using CodeWarsTests.Tasks;
+using NUnit.Framework;
+
+namespace CodeWarsTests.Tests.GeneralKataTests
+{
+    [TestFixture]
+    public class FindAverageTests
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            double[] array = new double[] { 17, 16, 16, 16, 16, 15, 17, 17, 15, 5, 17, 17, 16 };
+            Assert.AreEqual(200.0 / 13.0, Kata.FindAverage(array));
+        }
+
+        [Test]
+        public void EmptyTest()
+        {
+            Assert.AreEqual(0, Kata.FindAverage(new double[] { }));
+        }
+    }
+}
