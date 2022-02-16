cd364ad0f9565fd38ceca35fc1b84d85d0282ecd
taja01 <jarvas.tamas@gmail.com>
2022-02-17 00:05:08 +0100

    bit faster

--- SolutionForFun/test/CodeWarsTest/Tasks/LeetCode/Problems.cs
+++ SolutionForFun/test/CodeWarsTest/Tasks/LeetCode/Problems.cs
@@ -1,18 +1,23 @@
-﻿namespace CodeWarsTests.Tasks.LeetCode
+﻿using System.Collections.Generic;
+
+namespace CodeWarsTests.Tasks.LeetCode
 {
     public class Problems
     {
         public static int[] TwoSum(int[] nums, int target)
         {
+            var dict = new Dictionary<int, int>();
+
             for (int i = 0; i < nums.Length; i++)
             {
-                for (int j = i + 1; j < nums.Length; j++)
+                var current = nums[i];
+                var s = target - current;
+                if (dict.TryGetValue(s, out var index))
                 {
-                    if (nums[i] + nums[j] == target)
-                    {
-                        return new[] { i, j };
-                    }
+                    return new[] { index, i };
                 }
+
+                dict[current] = i;
             }
 
             return null;
