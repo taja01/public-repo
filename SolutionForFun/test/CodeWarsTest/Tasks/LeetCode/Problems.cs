ae92a4b24d15e5c0a3045f1b7ad00b5fc04346e2
taja01 <jarvas.tamas@gmail.com>
2022-02-16 23:58:10 +0100

    first version

---
+++ SolutionForFun/test/CodeWarsTest/Tasks/LeetCode/Problems.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeWarsTests.Tasks.LeetCode
+{
+    public class Problems
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new[] { i, j };
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ SolutionForFun/test/CodeWarsTest/Tests/LeetCodesTests/TwoSumTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWarsTests.Tasks.LeetCode;
+using NUnit.Framework;
+
+namespace CodeWarsTests.Tests.LeetCodesTests
+{
+    [TestFixture]
+    public class TwoSumTests
+    {
+        [Test]
+        public void Test0()
+        {
+            Assert.Null(Problems.TwoSum(new[] { 2, 7, 11, 15 }, 100));
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(new[] { 0, 1 }, Problems.TwoSum(new[] { 2, 7, 11, 15 }, 9));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(new[] { 1, 2 }, Problems.TwoSum(new[] { 3, 2, 4 }, 6));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(new[] { 0, 1 }, Problems.TwoSum(new[] { 3, 3 }, 6));
+        }
+    }
+}
