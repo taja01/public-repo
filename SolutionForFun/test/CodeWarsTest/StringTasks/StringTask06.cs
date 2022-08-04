1c3417c808be83631ac0392837f96f9a914fc820
taja01 <jarvas.tamas@gmail.com>
2022-08-04 20:10:40 +0200

    thursday

--- SolutionForFun/test/CodeWarsTest/StringTasks/StringTask06.cs
+++ SolutionForFun/test/CodeWarsTest/StringTasks/StringTask06.cs
@@ -42,5 +42,19 @@ namespace CodeWarsTests.StringTasks
 
             return str;
         }
+
+        //https://www.codewars.com/kata/595970246c9b8fa0a8000086
+        /* Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
+         * Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as intended (i.e. make the first character in the string "word" upper case).
+         * Don't worry about numbers, special characters, or non-string types being passed to the function. The string lengths will be from 1 character up to 10 characters, but will never be empty.
+         */
+        public static string CapitalizeWord(string word)
+        {
+            //var a = word.ToCharArray();
+            //a[0] = char.ToUpper(a[0]);
+            //return new string(a);
+
+            return char.ToUpper(word[0]) + word.Substring(1);
+        }
     }
 }
---
+++ SolutionForFun/test/CodeWarsTest/Tests/202208/CapitalizeWordTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWarsTests.StringTasks;
+using NUnit.Framework;
+
+namespace CodeWarsTests.Tests._202208
+{
+    [TestFixture]
+    public class CapitalizeWordTests
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Assert.AreEqual(Kata.CapitalizeWord("word"), "Word");
+            Assert.AreEqual(Kata.CapitalizeWord("i"), "I");
+            Assert.AreEqual(Kata.CapitalizeWord("glasswear"), "Glasswear");
+        }
+    }
+}
