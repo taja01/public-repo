0f16f0a1feba5a885d2c1ae3600b3516366927f8
taja01 <jarvas.tamas@gmail.com>
2023-03-01 08:18:55 +0100

    WednessDay

---
+++ SolutionForFun/test/CodeWarsTest/Tasks/202303/Practise202303.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CodeWarsTests.Tasks._202303 {
+    internal class Practise202303 {
+
+        //https://www.codewars.com/kata/58d248c7012397a81800005c/train/csharp
+        public static bool IsCube(double volume, double side) {
+            //if (volume < 1 || side < 1) return false;
+
+            //return side * side * side == volume;
+
+            return Math.Pow(side, 3) == volume && volume > 0 && side > 0;
+        }
+    }
+}
---
+++ SolutionForFun/test/CodeWarsTest/Tests/202303/IsCubeTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWarsTests.Tasks._202303;
+using NUnit.Framework;
+
+namespace CodeWarsTests.Tests._202303 {
+
+    [TestFixture]
+    internal class IsCubeTests {
+        [Test]
+        public void ShouldReturnFalse() {
+
+            Assert.AreEqual(true, Practise202303.IsCube(1, 1));
+            Assert.AreEqual(true, Practise202303.IsCube(8, 2));
+        }
+        [Test]
+        public void ShouldReturnTrue() {
+
+            Assert.AreEqual(false, Practise202303.IsCube(2, 1));
+            Assert.AreEqual(false, Practise202303.IsCube(6, 3));
+            Assert.AreEqual(false, Practise202303.IsCube(-8, -2));
+            Assert.AreEqual(false, Practise202303.IsCube(0, 0));
+            Assert.AreEqual(false, Practise202303.IsCube(200, 4));
+        }
+    }
+}
