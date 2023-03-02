d76cb356edb1408f586ce7dab3975d541b225886
taja01 <jarvas.tamas@gmail.com>
2023-03-02 09:29:35 +0100

    Thursday

--- SolutionForFun/test/CodeWarsTest/Tasks/202303/Practise202303.cs
+++ SolutionForFun/test/CodeWarsTest/Tasks/202303/Practise202303.cs
@@ -11,5 +11,14 @@ namespace CodeWarsTests.Tasks._202303 {
 
             return Math.Pow(side, 3) == volume && volume > 0 && side > 0;
         }
+
+        //https://www.codewars.com/kata/5ab6538b379d20ad880000ab/train/csharp
+        /*
+         * You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+         * If it is a square, return its area. If it is a rectangle, return its perimeter.
+         */
+        public static int AreaOrPerimeter(int l, int w) {
+            return l == w ? l * w : 2 * (l + w);
+        }
     }
 }
---
+++ SolutionForFun/test/CodeWarsTest/Tests/202303/AreaOrPerimeterTests.cs
@@ -0,0 +1,14 @@
+﻿using CodeWarsTests.Tasks._202303;
+using NUnit.Framework;
+
+namespace CodeWarsTests.Tests._202303 {
+
+    [TestFixture]
+    internal class AreaOrPerimeterTests {
+        [Test]
+        public void FixedTest() {
+            Assert.AreEqual(Practise202303.AreaOrPerimeter(4, 4), 16);
+            Assert.AreEqual(Practise202303.AreaOrPerimeter(6, 10), 32);
+        }
+    }
+}
