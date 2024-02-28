1162bc02c24ba025f04116d965d86d5f5b5c51c0
taja01 <jarvas.tamas@gmail.com>
2024-02-28 08:41:30 +0100

    start again, daily test

---
+++ LetCode/LetCode/Tasks/Task001.cs
@@ -0,0 +1,19 @@
+﻿namespace LetCode.Tasks
+{
+    internal class Task001
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                        return [i, j];
+                }
+            }
+
+            return new int[0];
+        }
+    }
+}
---
+++ LetCode/LetCode/Tests/Task001Tests.cs
@@ -0,0 +1,34 @@
+﻿using LetCode.Tasks;
+
+namespace LetCode.Tests
+{
+    internal class Task001Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var numbers = new[] { 2, 7, 11, 15 };
+            var result = Task001.TwoSum(numbers, 9);
+
+            Assert.That(result, Is.EqualTo(new[] { 0, 1 }));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var numbers = new[] { 3, 3 };
+            var result = Task001.TwoSum(numbers, 6);
+
+            Assert.That(result, Is.EqualTo(new[] { 0, 1 }));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var numbers = new[] { 1, 5, 3 };
+            var result = Task001.TwoSum(numbers, 5);
+
+            Assert.That(result, Is.EqualTo(new int[] { }));
+        }
+    }
+}
