6905018d888728b1d6835be73032567944ed4638
taja01 <jarvas.tamas@gmail.com>
2024-02-28 08:28:21 +0100

    adding fluent validation project todo: fill it

---
+++ Examples/FluentValidators/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FluentValidators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
