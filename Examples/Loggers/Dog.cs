27e34b9eb89db2bda69f382135e97ed8cbdb7536
taja01 <jarvas.tamas@gmail.com>
2024-02-28 08:27:59 +0100

    log example

---
+++ Examples/Loggers/Dog.cs
@@ -0,0 +1,21 @@
+﻿namespace Loggers
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Breed { get; set; }
+
+        public Dog(string name, string color, string breed)
+        {
+            Name = name;
+            Color = color;
+            Breed = breed;
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine($"{Name} is barking.");
+        }
+    }
+}
---
+++ Examples/Loggers/Program.cs
@@ -0,0 +1,37 @@
+﻿using Loggers;
+using Serilog;
+
+namespace Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var loggerConfig = new LoggerConfiguration();
+
+            loggerConfig
+                .MinimumLevel.Debug()
+                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day);
+
+            var logger = loggerConfig.CreateLogger();
+
+            logger.Information("Program Start");
+
+            var dog = new Dog("Rex", "Black", "GermanSheppard");
+
+            logger.Warning("Dog created: {@dog}, output dog: {@dog}", dog, dog);
+
+            try
+            {
+                var wrongIndex = dog.Name[100];
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Exception throw");
+            }
+
+            logger.Information(messageTemplate: "Program End");
+        }
+    }
+}
